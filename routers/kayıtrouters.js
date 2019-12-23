babad96ec0dcbb0b2922c323ea55794e7596234a
Mankurt543 <55107569+Mankurt543@users.noreply.github.com>
2019-12-23 09:21:32 +0300

    ilexa

--- routers/kayıtrouters.js
+++ routers/kayıtrouters.js
@@ -3,7 +3,7 @@ var router=express.Router();
 var kayıtcontrollers=require("../controller/kayıtcontroller")
 
 router.get("/kayit/:thisRemove",kayıtcontrollers.removeThisRecord)
-router.get("/kayit",kayıtcontrollers.kayıtekranı)
+router.get("/",kayıtcontrollers.kayıtekranı)
 router.post("/records",kayıtcontrollers.kayitlar)
 
 module.exports=router;
